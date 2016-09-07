d633dcd8304e7c18f48db2243ab5dbc4037bbaad
gauchoaleman <gauchoaleman@yahoo.com>
2016-09-07 12:58:57 -0300

    1er commit con ejercicios.

--- src/edu/capacitas/Stefan/CalculadoraBasica.java
+++ src/edu/capacitas/Stefan/CalculadoraBasica.java
@@ -6,28 +6,93 @@ public class CalculadoraBasica {
 
     public static void main(String args[]) {
         Scanner operador = new Scanner(System.in);
-        mostrarMenu();
-        int operacion = operador.nextInt();
-        if (!(operacion >= 1 && operacion <= 4)) {
-            System.out.println("Operación inválida");
-        } else {
-            int resultado=0;
-            System.out.println("Ingrese operando 1");
-            int operando1 = operador.nextInt();
-            System.out.println("Ingrese operando 2");
-            int operando2 = operador.nextInt();
-            if (operacion == 1) {
-                resultado = operando1 + operando2;
-            } else if (operacion == 2){
-            resultado = operando1 - operando2;
-            }else if( operacion == 3) {
-                resultado = operando1 * operando2;
-            }else if( operacion == 4) {
-                resultado = operando1 / operando2;
+
+        String operacion;
+        String otracuenta;
+        int repetir=1;
+        int operacionint=0;
+        int validoperacion=1;
+
+        do{
+
+        do {
+            mostrarMenu();
+            System.out.println("Ingrese operacion:");
+            operacion = operador.nextLine();
+            if (!isNumeric(operacion) ) {
+                System.out.println("Operación inválida");
+                validoperacion = 0;
+            }else {
+                operacionint = Integer.parseInt(operacion);
+                if (!isNumeric(operacion) || !(operacionint >= 1 && operacionint <= 4)) {
+                    System.out.println("Operación inválida");
+                    validoperacion = 0;
+                } else {
+                    validoperacion = 1;
+                }
             }
-            System.out.println("Resultado: " + resultado);
-        }
-    }
+        }while(validoperacion==0 );
+
+            float resultado=0;
+            int validop1=1;
+            int validop2=1;
+            String operando1s;
+            String operando2s;
+            float operando1;
+            float operando2;
+
+            do {
+                System.out.println("Ingrese operando 1");
+                operando1s = operador.nextLine();
+
+                if (!isNumeric(operando1s)) {
+                    System.out.println("Operando debe ser número");
+                    validop1 = 0;
+                }
+                else {
+                    validop1 = 1;
+                }
+
+            }   while(validop1==0 );
+                    do {
+                        System.out.println("Ingrese operando 2");
+                        operando2s = operador.nextLine();
+                        if (!isNumeric(operando2s)) {
+                            System.out.println("Operando debe ser número");
+                            validop2 = 0;
+                        }
+                        else {
+                            validop2 = 1;
+                        }
+                    } while (validop2 == 0);
+
+            operando1= Float.parseFloat(operando1s);
+            operando2= Float.parseFloat(operando2s);
+            if (operacionint == 1) {
+                resultado = operando1 + operando2;
+                System.out.println("Resultado: " + resultado);
+            } else if (operacionint == 2){
+                resultado = operando1 - operando2;
+                System.out.println("Resultado: " + resultado);
+            }else if( operacionint == 3) {
+                resultado = operando1 * operando2;
+                System.out.println("Resultado: " + resultado);
+            }else if( operacionint == 4) {
+                if( operando2==0 ) {
+                    System.out.println("No se puede calcular división por cero");
+                }
+                else {
+                    resultado = operando1 / operando2;
+                    System.out.println("Resultado: " + resultado);
+                }
+            }
+            System.out.println("Desea hacer otra cuenta? (s/n)");
+            otracuenta = operador.nextLine();
+            if( otracuenta.equals("n")){
+                repetir = 0;
+            }
+        }while (repetir==1);
+}
 
     public static void mostrarMenu() {
         System.out.println("Calculadora básica:");
@@ -35,6 +100,20 @@ public class CalculadoraBasica {
         System.out.println("2) Restar");
         System.out.println("3) Multiplicar");
         System.out.println("4) Dividir:");
-        System.out.println("Ingrese operación");
+    }
+    private static boolean isNumeric(String cadena){
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
     }
 }
+
+
+
+
+
+
+    // Función para validar un número
