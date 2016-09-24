f5b570b174377026a7d1e6cbb5316ef47c1a7f80
gauchoaleman <gauchoaleman@yahoo.com>
2016-09-24 17:45:05 -0300

    Ejemplo de Clases y Objetos

---
+++ src/edu/capacitas/Stefan/persona/Main.java
@@ -0,0 +1,15 @@
+package edu.capacitas.Stefan.persona;
+
+/**
+ * Created by Stefan on 24/09/2016.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Stefan");
+        System.out.println(persona.getNombre());
+
+    }
+}
