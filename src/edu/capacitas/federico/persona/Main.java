f6b7599af38c44a5ba84b8070166d4da72dea80b
Fede <fedee_13_bordini@hotmail.com>
2016-09-30 12:53:00 -0300

    primer commed paquete federico

--- src/edu/capacitas/federico/persona/Main.java
+++ src/edu/capacitas/federico/persona/Main.java
@@ -1,12 +1,41 @@
 package edu.capacitas.federico.persona;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Fede on 26/09/2016.
  */
 public class Main {
+
     public static void main(String[] args ) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaNacimiento;
+        try {
+            fechaNacimiento = dateFormat.parse("17/08/1995");
+        } catch (ParseException e) {
+            fechaNacimiento = null;
+        }
         Persona persona = new Persona();
         persona.setNombre("Federico");
-        System.out.print(persona.getNombre());
+        persona.setApellido("Bordini");
+        persona.setDni("38940065");
+        persona.setTelefono("21354339");
+        persona.setEdad(21);
+        persona.setFechaNacimiento(fechaNacimiento);
+
+        System.out.println("valor de variables");
+
+        System.out.println(persona.getNombre());
+
+        System.out.println(persona.getApellido());
+        System.out.println(persona.getDni());
+        System.out.println(persona.getTelefono());
+        System.out.println(persona.getEdad());
+        Date fecha = persona.getFechaNacimiento();
+        System.out.println(dateFormat.format(fecha));
+
+
     }
 }
--- src/edu/capacitas/federico/persona/Persona.java
+++ src/edu/capacitas/federico/persona/Persona.java
@@ -1,16 +1,63 @@
 package edu.capacitas.federico.persona;
 
+import java.util.Date;
+
 /**
  * Created by Fede on 26/09/2016.
  */
 public class Persona {
-
     private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private int edad;
+    private Date fechaNacimiento;
 
-    public String getNombre() {return nombre;
+    public String getNombre() {
+        return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 }
